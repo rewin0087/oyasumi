b590fa18db9672a54a8d17d183a375aeb9d75293
Erwin Seribo <erwin_rulezzz87@yahoo.com>
2023-03-14 22:13:53 +0800

    setup simple auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+  include JSONAPI::ActsAsResourceController
+
+  before_action :authenticate_user!
+
+  attr_reader :current_user
+
+   rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { data: { message: e.message } }, status: 404
+  end
+
+  private
+
+  def context
+    { current_user: current_user }
+  end
+
+  def authenticate_user!
+    uuid = request.headers['HTTP_X_UID']
+
+    @current_user = User.find_by(uuid: uuid)
+
+    return render json: { message: 'Unauthorized' }, status: :unauthorized unless @current_user
+  end
 end
